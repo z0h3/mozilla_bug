b06baea53762478347615b56f9e3456447dcb884
Markus Schulz <msc@onesty-tech.de>
2023-01-18 10:32:46 +0100

    Test-case extended to check more same-site options

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+const value = ""+Math.random();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.cookie("mycookie", "value", { httpOnly: true, sameSite: "strict" })
-  res.send("<iframe srcdoc=\"<html><script>const req = new XMLHttpRequest(); req.open('get', '/check_cookie'); req.send()</script></html>\" title=\"\"></iframe>")
+  res.cookie("laxCookie", value, { sameSite: "lax" })
+  res.cookie("strictCookie", value, { sameSite: "strict" })
+  res.cookie("noneCookie", value, { sameSite: "none" })//none requires ssl for chrome...
+  res.cookie("defaultCookie", value, { })
+  res.send('<!DOCTYPE html><html lang=\'de\'><body><iframe srcdoc="<!DOCTYPE html><html><script>fetch(\'/check_cookie\').then(res => res.text()).then(res => document.body.innerHTML =res);</script></html>" title=""></iframe></body></html>')
 });
 
 router.get('/check_cookie', function(req, res, next) {
   console.info("request cookie", req.cookies)
-  res.end()
+  var content = req.cookies["laxCookie"] === value ?
+        '<span style="color:green">1.lax-PASS</span><br/>' :
+        '<span style="color:red">1.lax-FAIL</span><br/>';
+  content += req.cookies["strictCookie"] === value ?
+        '<span style="color:green">2.strict-PASS</span><br/>' :
+        '<span style="color:red">2.strict-FAIL</span><br/>';
+  content += req.cookies["noneCookie"] === value ?
+        '<span style="color:green">3.none-PASS(SSL-Required)</span><br/>' :
+        '<span style="color:red">3.none-FAIL(SSL-Required)</span><br/>';
+  content += req.cookies["defaultCookie"] === value ?
+        '<span style="color:green">4.def-PASS</span><br/>' :
+        '<span style="color:red">4.def-FAIL</span><br/>';
+  res.end(content)
 });
 
 module.exports = router;
